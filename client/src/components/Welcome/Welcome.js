27b8cf38621eb7c6dff6d7e04133e04c85a8b98e
doleksiyenko <dan.oleksiyenko@gmail.com>
2020-06-22 16:52:57 -0400

    Updated welcome page styling

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -11,42 +11,47 @@ const Welcome = () => {
     let [warning, showWarning] = useState(false);
 
     return (
-        <div id="sectionCard">
-            <h1>
-                Welcome. <span>Join A Room.</span>
-            </h1>
-            <form>
-                <input
-                    placeholder="Enter Room Name"
-                    onChange={(event) => setRoom(event.target.value)}
-                ></input>{" "}
-                <br></br>
-                <input
-                    placeholder="Enter Username"
-                    onChange={(event) => setName(event.target.value)}
-                ></input>{" "}
-                <br></br>
-                {/* when we link, need to check whether the room/name entry is empty */}
-                {room.length > 0 && name.length > 0 ? (
-                    <Link
-                        to={{
-                            pathname: "/room",
-                            search: `?room=${room}&name=${name}`,
-                        }}
-                    >
-                        <Button className="btn btn-primary" type="submit">
+        <div id="welcome-body">
+            <div id="sectionCard">
+                <h1>
+                    Welcome. <span>Join A Room.</span>
+                </h1>
+                <form>
+                    <input
+                        placeholder="Enter Room Name"
+                        onChange={(event) => setRoom(event.target.value)}
+                    ></input>{" "}
+                    <br></br>
+                    <input
+                        placeholder="Enter Username"
+                        onChange={(event) => setName(event.target.value)}
+                    ></input>{" "}
+                    <br></br>
+                    {/* when we link, need to check whether the room/name entry is empty */}
+                    {room.length > 0 && name.length > 0 ? (
+                        <Link
+                            to={{
+                                pathname: "/room",
+                                search: `?room=${room}&name=${name}`,
+                            }}
+                        >
+                            <Button
+                                className="btn btn-primary join-button"
+                                type="submit"
+                            >
+                                Join
+                            </Button>
+                        </Link>
+                    ) : (
+                        <Button
+                            className="disabled join-button"
+                            onClick={() => showWarning(true)}
+                        >
                             Join
                         </Button>
-                    </Link>
-                ) : (
-                    <Button
-                        className="disabled"
-                        onClick={() => showWarning(true)}
-                    >
-                        Join
-                    </Button>
-                )}
-            </form>
+                    )}
+                </form>
+            </div>
         </div>
     );
 };
