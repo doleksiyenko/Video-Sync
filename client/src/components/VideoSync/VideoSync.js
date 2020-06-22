c100e63c85fa21514af8aa61732b031642307b47
doleksiyenko <dan.oleksiyenko@gmail.com>
2020-06-22 17:57:15 -0400

    Client socket establish connection with server

--- client/src/components/VideoSync/VideoSync.js
+++ client/src/components/VideoSync/VideoSync.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import "./VideoSync.css";
 
+//modules
+import io from "socket.io-client";
+import queryString from "query-string";
+
 const VideoSync = () => {
+    const serverLocation = "localhost:5000";
+
+    useEffect(() => {
+        let user = queryString.parse(window.location.search);
+        // once the params have been retrieved, set the document title to the room name
+        document.title = `Video Sync - Room ${user.room}`;
+
+        const socket = io(serverLocation);
+        console.log(socket);
+    }, [window.location.search, serverLocation]);
     return <h1>Video Sync</h1>;
 };
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const io = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
     res.send(`Express loaded on port ${port}`);
+});
+
+// socket.io
+
+io.on("connection", (socket) => {
+    console.log("A user has connected.");
 });
 
 http.listen(port, () => {
